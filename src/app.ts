3badb64271cadd844f9d53bf8bd82c47fa807cd2
Matěj Wiedermann <matej.wiedermann@plus4u.net>
2025-03-12 22:58:24 +0100

    first addition

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
 import dotenv from "dotenv";
 import app from "./routes/main";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My Express.js API',
+      version: '1.0.0',
+      description: 'A sample Express.js API built with TypeScript and Swagger',
+    },
+  },
+  apis: ['./src/routes/*.ts'],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -4,8 +4,29 @@ import { Greeter } from "../services/main";
 export const app = express();
 const greeter = new Greeter();
 
+
+/**
+ * @openapi
+ * /hello:
+ *   get:
+ *     summary: Returns a greeting message
+ *     description: Returns a simple "Hello World!" greeting message
+ *     responses:
+ *       200:
+ *         description: A successful response containing a greeting message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Hello World!"
+ */
 app.get("/hello", (req: Request, res: Response) => {
   res.json({ message: greeter.sayHi() });
 });
+
+
 
 export default app;
