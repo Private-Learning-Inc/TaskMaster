146abb9d9ea0e4542cc6d391106a26e9c2544ecb
Jan Novak <jan.novak.2@plus4u.net>
2025-03-13 04:39:57 +0100

    chore: cleanup of configurations in app.ts to config.ts

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,17 @@
 import dotenv from "dotenv";
 import app from "./routes/main";
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import { Config } from "./config";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'My Express.js API',
-      version: '1.0.0',
-      description: 'A sample Express.js API built with TypeScript and Swagger',
-    },
-  },
-  apis: ['./src/routes/*.ts'],
-};
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerJsdoc(Config.swaggerOptions)),
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
